399f854895685a694301ce2712e914a9a0b1bf7c
Eason(G Ray) <30045503+Eason0729@users.noreply.github.com>
2025-02-15 04:03:54 +0000

    fix: :bug: rename column

--- data/init.sql
+++ data/init.sql
@@ -33,17 +33,16 @@ CREATE TABLE FIN_Data_csv AS
 SELECT * FROM read_csv('FIN_Data.csv');
 CREATE TABLE FIN_Data_raw AS
 SELECT
-    f.CALENDAR_YEAR,
+    f.CALENDAR_YEAR as CalendarYear,
     f."Company Name" as CompanyName,
     c.Country,
-    f.CALENDAR_QTR,
-    SUM(CASE WHEN f.Index = 'Cost of Goods Sold' THEN f.USD_Value ELSE NULL END) AS "Cost of Goods Sold",
-    SUM(CASE WHEN f.Index = 'Operating Expense' THEN f.USD_Value ELSE NULL END) AS "Operating Expense",
-    SUM(CASE WHEN f.Index = 'Operating Income' THEN f.USD_Value ELSE NULL END) AS "Operating Income",
-    SUM(CASE WHEN f.Index = 'Revenue' THEN f.USD_Value ELSE NULL END) AS "Revenue",
-    SUM(CASE WHEN f.Index = 'Tax Expense' THEN f.USD_Value ELSE NULL END) AS "Tax Expense",
-    SUM(CASE WHEN f.Index = 'Total Asset' THEN f.USD_Value ELSE NULL END) AS "Total Asset"
-  
+    f.CALENDAR_QTR as CalendarQuarter,
+    SUM(CASE WHEN f.Index = 'Cost of Goods Sold' THEN f.USD_Value ELSE NULL END) AS CostOfGoodsSold,
+    SUM(CASE WHEN f.Index = 'Operating Expense' THEN f.USD_Value ELSE NULL END) AS OperatingExpense,
+    SUM(CASE WHEN f.Index = 'Operating Income' THEN f.USD_Value ELSE NULL END) AS OperatingIncome,
+    SUM(CASE WHEN f.Index = 'Revenue' THEN f.USD_Value ELSE NULL END) AS Revenue,
+    SUM(CASE WHEN f.Index = 'Tax Expense' THEN f.USD_Value ELSE NULL END) AS TaxExpense,
+    SUM(CASE WHEN f.Index = 'Total Asset' THEN f.USD_Value ELSE NULL END) AS TotalAsset
 FROM
     FIN_Data_csv f
 LEFT JOIN
@@ -70,8 +69,8 @@ FROM
 CREATE TABLE TRANSCRIPT_Data_raw AS
 SELECT
     TDR."Company Name" AS CompanyName,
-    TDR.CALENDAR_YEAR,
-    TDR.CALENDAR_QTR,
+    TDR.CALENDAR_YEAR as CalendarYear,
+    TDR.CALENDAR_QTR as CalendarQuarter,
     CR.Country,
     TFR.filename,
     TFR.content
@@ -84,48 +83,48 @@ JOIN
 
 CREATE TABLE FIN_Data_Derived_raw AS
 SELECT
-    CALENDAR_YEAR,
+    CalendarYear,
     CompanyName,
     Country,
-    CALENDAR_QTR,
+    CalendarQuarter,
     "Revenue",
-    "Cost of Goods Sold",
-    "Operating Expense",
-    "Operating Income",
-    "Tax Expense",
-    "Total Asset",
-    ("Revenue" - "Cost of Goods Sold") AS Gross_Profit,
+    "CostOfGoodsSold",
+    "OperatingExpense",
+    "OperatingIncome",
+    "TaxExpense",
+    "TotalAsset",
+    ("Revenue" - "CostofGoodsSold") AS GrossProfit,
     CASE
         WHEN "Revenue" = 0 THEN NULL
-        ELSE ("Revenue" - "Cost of Goods Sold") / "Revenue" * 100
-    END AS Gross_Profit_Margin,
+        ELSE ("Revenue" - "CostOfGoodsSold") / "Revenue" * 100
+    END AS GrossProfitMargin,
     CASE
         WHEN "Revenue" = 0 THEN NULL
-        ELSE "Operating Income" / "Revenue" * 100
-    END AS Operating_Profit_Margin,
-    ("Operating Income" - "Tax Expense") AS Net_Income_Before_Tax,
+        ELSE "OperatingIncome" / "Revenue" * 100
+    END AS OperatingProfitMargin,
+    ("OperatingIncome" - "TaxExpense") AS NetIncomeBeforeTax,
     CASE
         WHEN "Revenue" = 0 THEN NULL
-        ELSE ("Operating Income" - "Tax Expense") / "Revenue" * 100
-    END AS Net_Profit_Margin_Before_Tax,
+        ELSE ("OperatingIncome" - "TaxExpense") / "Revenue" * 100
+    END AS NetProfitMarginBeforeTax,
     CASE
-        WHEN "Operating Income" = 0 THEN NULL
-        ELSE ("Tax Expense" / "Operating Income") * 100
-    END AS Effective_Tax_Rate,
+        WHEN "OperatingIncome" = 0 THEN NULL
+        ELSE ("TaxExpense" / "OperatingIncome") * 100
+    END AS EffectiveTaxRate,
     CASE
-        WHEN "Total Asset" = 0 THEN NULL
-        ELSE ("Operating Income" - "Tax Expense") / "Total Asset" * 100
-    END AS Return_on_Assets
+        WHEN "TotalAsset" = 0 THEN NULL
+        ELSE ("OperatingIncome" - "TaxExpense") / "TotalAsset" * 100
+    END AS ReturnOnAssets
 FROM
     FIN_Data_raw;
 
 CREATE TABLE Exchange_Rate_raw 
 AS
 SELECT
-    CALENDAR_YEAR,
-    CALENDAR_QTR,
-    Local_Currency,
-    SUM(USD_Value) / SUM(Local_Value) AS Exchange_Rate
+    CALENDAR_YEAR as CalendarYear,
+    CALENDAR_QTR as CalendarQuarter,
+    Local_Currency as LocalCurrency,
+    SUM(USD_Value) / SUM(Local_Value) AS ExchangeRate
 FROM
     FIN_Data_csv
 GROUP BY
@@ -136,11 +135,11 @@ GROUP BY
 CREATE TABLE Fiscal_Data_raw AS
 SELECT DISTINCT
     "Company Name" as CompanyName,
-    CALENDAR_YEAR,
-    CALENDAR_QTR,
+    CALENDAR_YEAR as CalenderYear,
+    CALENDAR_QTR as CalenderQuarter,
     COALESCE(
         TRY_CAST(REGEXP_EXTRACT(Transcript_Filename, ' Q[1-4] (\d{4})', 1) AS BIGINT),
         CALENDAR_YEAR
-    ) AS CALENDAR_YEAR,
-    REGEXP_EXTRACT(Transcript_Filename, ' (Q[1-4]) ', 1) AS FISCAL_QTR
+    ) AS FiscalYear,
+    REGEXP_EXTRACT(Transcript_Filename, ' (Q[1-4]) ', 1) AS FiscalQuarter
 FROM TRANSCRIPT_Data_csv;
